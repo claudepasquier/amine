3e5e4cfadb47e1a662f6e43fc01274bae1e32b42
Claude Pasquier <claude.pasquier@unice.fr>
2023-03-31 17:03:07 +0200

    Added default program parameters

--- amine/parameters.py
+++ amine/parameters.py
@@ -23,16 +23,23 @@ import pathlib
 class Param:
     """Group program parameters."""
 
+    # name of the project
     project_name = "amine"
 
+    # version of the STRING database
     string_version = "11.5"
+
+    # version of the BioGRID database
     biogrid_version = "4.4.216"
 
-    # Datasets are located in 'datadir'
+
+    # Directory where the datasets are stored
+    # by default it is the directory named data on the project root directory
     datadir = pathlib.Path(pathlib.Path(__file__).parent.parent, "data")
 
     precomputed_models_dir = pathlib.Path(datadir, "models")
 
+    # Specification of all the species handled by the program with their associated id
     specie2id = {
         "human": "9606",
         "mouse": "10090",
@@ -40,11 +47,53 @@ class Param:
         "Caenorhabditis elegans": "6239",
     }
 
+    # Specification of the default specie to process
+    # It must correspond to a key used in specie2id
+    default_specie = "human"
+
+    # Specification of the default network to use that can be
+    # 'string', 'biogrid', 'intact' or the path to a network file in edge list format
+    default_network = "string"
+
+    #
+    # Filters to be applied to the STRING database
+    # this correspond to the different composants of the evidence score
+    #
+    ppi_string_graph ={
+        "min_neighborhood": 0,   "min_fusion": 0, "min_cooccurence" : 0,
+        "min_coexpression": 0, "min_experimental": 0, "min_database" : 0,
+        "min_textmining": 0, "min_combined_score" : 0}
+
+    #
+    # Filter to be applied to the BioGRID database
+    # There is only one choice, corresponding to the type of interaction
+    # that can be 'physical', 'genetic' or None is no filter is used
+    #
+    ppi_biogrid_graph ={
+        "inter_type": None}
+
+    #
+    # Filter to be applied to the Intact database
+    # This correspond to the minimum confidence score
+    #
+    ppi_intact_graph = {
+        "min_confidence": 0.0}
+
+    #
+    # Parameters and Filter to be used when reading a custom graph in edgelist format
+    #
+    ppi_custom_graph ={
+        "path": None,
+        "source_col": 0,
+        "target_col": 1,
+        "header": None # Row number to use as the column names    
+        }
+
+    verbose = False
+
     go_file = os.path.join(datadir, "go-basic.obo")
     goslim_file = os.path.join(datadir, "goslim_generic.obo")
     gene2go_file = os.path.join(datadir, "gene2go")
-
-    verbose = False
 
     @staticmethod
     def get_ncbi_background_genes(specie, extension: str = "txt"):
